bb5b1b59c26e7bdf86f73703dc0e54582deb26a8
dogfoot <dogfoot_log@naver.com>
2022-05-03 22:54:06 +0900

    git push test

--- src/component/checkbox/Checkbox.jsx
+++ src/component/checkbox/Checkbox.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { bool, func, string } from "prop-types";
 import "./Checkbox.css";
 
+// git push test
 const Checkbox = ({ checked = false, labelText = "", onClick = null }) => {
   return (
     <div className="checkbox-div">
