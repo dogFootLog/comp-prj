4b2efeac17f82c47ed46da1c7e5d2341442272a7
dogfoot_log <dogfoot_log@naver.com>
2022-05-11 18:43:34 +0900

    react-spring-bottom-sheet react-dom@17.0.2 TEST

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import React, { useState } from "react";
+import { BottomSheet } from "react-spring-bottom-sheet";
 import "./App.css";
-import Routes from "@/routes/Routes";
-import ModalRedux from "./component/modalRedux/ModalRedux";
 
 function App() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
-      <ModalRedux />
-    </div>
+    <>
+      <div className="main">
+        MAIN CONTENT
+        <button type="button" onClick={() => setOpen(!open)}>
+          {open ? "CLOSE" : "OPEN"} SHEET
+        </button>
+      </div>
+      <div className="footer">YOUR MAIN FOOTER</div>
+      <BottomSheet
+        open={open}
+        onDismiss={() => setOpen(false)}
+        header={<div className="sheetHeader">SHEET HEADER</div>}
+        snapPoints={({ maxHeight }) => 0.7 * maxHeight}
+        sibling={<div className="sheetFooter">YOUR MAIN FOOTER</div>}
+      >
+        <div className="sheetBody">SHEET BODY</div>
+      </BottomSheet>
+    </>
   );
 }
 
