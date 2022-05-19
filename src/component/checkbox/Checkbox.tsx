c41c1c72adb95bdf00897ff3d4ddcd3827269de9
dogfoot <dogfoot_log@naver.com>
2022-05-19 23:44:32 +0900

    [220519] checkbox.tsx 보완

--- src/component/checkbox/Checkbox.tsx
+++ src/component/checkbox/Checkbox.tsx
@@ -4,16 +4,16 @@ import "./Checkbox.css";
 interface propsType {
   checked: boolean;
   labelText: string;
-  onClick(e: React.ChangeEvent<HTMLInputElement>): void;
+  onClick(): void;
 }
 const Checkbox = ({
   checked = false,
   labelText = "",
-  onClick = () => {},
+  onClick = (/* e: React.ChangeEvent<HTMLInputElement> */) => {},
 }: propsType) => {
   return (
     <div className="checkbox-div">
-      <input type="checkbox" checked={checked} onChange={(e) => onClick(e)} />{" "}
+      <input type="checkbox" checked={checked} onChange={onClick} />
       {labelText}
     </div>
   );
